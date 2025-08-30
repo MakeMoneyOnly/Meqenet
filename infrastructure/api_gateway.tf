f87b194daea263df00dd5e61244d7a4a64f680e7
Meqenet Development Team <admin@meqenet.et>
2025-08-30 17:30:13 +0300

    fix: address Terraform security issues from tfsec

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -13,7 +13,7 @@ resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.main.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
-  authorization = "NONE"
+  authorization = "AWS_IAM"
 }
 
 resource "aws_api_gateway_integration" "proxy" {
--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -125,10 +125,19 @@ resource "aws_security_group" "default" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    self        = true
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+    description = "Allow HTTP traffic from VPC"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+    description = "Allow HTTPS traffic from VPC"
   }
 
   egress {
